77b18483a65c376d6d94bcc4e17fe9ac11efd627
Eric Lifka <eric.lifka@inin.com>
2016-11-23 21:16:23 -0500

    use pixi in the simplest way possible to put something on the screen

--- game/main.js
+++ game/main.js
@@ -1,11 +1,27 @@
 SM.DefineModule('main', function (require) {
     const RunLoop = require('util/run-loop');
-    const PIXI = require('util/pixi');
+    const PIXI = require('pixi');
+
+    let renderer = new PIXI.WebGLRenderer(800, 600);
+    document.body.appendChild(renderer.view);
+
+    let stage = new PIXI.Container();
+
+    let bunny = null;
 
     let loop = new RunLoop(function (dtime) {
-        console.log(dtime);
+        bunny.rotation += 0.01;
+        renderer.render(stage);
     });
 
-    loop.start();
+    PIXI.loader.add('bunny', 'assets/bunny.gif').load(function (loader, resources) {
+        bunny = new PIXI.Sprite(resources.bunny.texture);
+        bunny.position.x = 400;
+        bunny.position.y = 300;
+        bunny.scale.x = 2;
+        bunny.scale.y = 2;
+        stage.addChild(bunny);
 
+        loop.start();
+    });
 });
