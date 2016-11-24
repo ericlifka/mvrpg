b23820df2f9f7d01484c10c47583792609029a8f
Eric Lifka <eric.lifka@inin.com>
2016-11-23 21:09:25 -0500

    add pixi.js as a dependency

--- game/main.js
+++ game/main.js
@@ -1,5 +1,6 @@
 SM.DefineModule('main', function (require) {
     const RunLoop = require('util/run-loop');
+    const PIXI = require('util/pixi');
 
     let loop = new RunLoop(function (dtime) {
         console.log(dtime);
