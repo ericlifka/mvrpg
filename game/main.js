4b6ec8f460d33720fd7425697b534f7fdae2651c
Eric Lifka <eric.lifka@inin.com>
2016-12-06 19:52:59 -0500

    giant pile of rubish

--- game/main.js
+++ game/main.js
@@ -3,6 +3,7 @@ SM.DefineModule('main', function (require) {
     const PIXI = require('pixi');
 
     let renderer = new PIXI.WebGLRenderer(800, 600);
+    renderer.backgroundColor = "0xffffff";
     document.body.appendChild(renderer.view);
 
     let stage = new PIXI.Container();
@@ -12,22 +13,56 @@ SM.DefineModule('main', function (require) {
 
     // let bunny = null;
     let box = new PIXI.Graphics();
-    box.lineStyle(5, "0xffffff", 1.0);
-    box.beginFill("0x0000ff", 1.0);
     // box.drawShape(new PIXI.Rectangle(0, 0, 100, 200));
 
+    // box.drawPolygon([
+    //     550, 100, // Starting x, y coordinates for the star
+    //     570, 150, // Star is drawn in a clockwork motion
+    //     630, 155,
+    //     585, 195,
+    //     600, 250,
+    //     550, 220,
+    //     500, 250,
+    //     515, 195,
+    //     470, 155,
+    //     530, 150,
+    //     550, 100
+    // ]);
+    box.position.x = 50;
+    box.position.y = 50;
+
+    box.beginFill("0x85e3f2", 1.0);
+    box.lineStyle(2, "0x000000", 1.0);
     box.drawPolygon([
-        550, 100, // Starting x, y coordinates for the star
-        570, 150, // Star is drawn in a clockwork motion
-        630, 155,
-        585, 195,
-        600, 250,
-        550, 220,
-        500, 250,
-        515, 195,
-        470, 155,
-        530, 150,
-        550, 100
+        25, 25,
+        0, 225,
+        100, 275,
+        50, 175,
+        25, 25
+    ]);
+    box.endFill();
+
+    box.beginFill("0xffe0bd", 1.0);
+    box.lineStyle(3, "0x000000", 1.0);
+    box.drawPolygon([
+        10, 0,
+        5, 55,
+        50, 65,
+        30, 10,
+        10, 0
+    ]);
+    box.endFill();
+
+    box.beginFill('0xff602b', 1.0);
+    box.lineStyle(0);
+    box.drawPolygon([
+        10, 0,
+        25, 25,
+        35, 70,
+        55, 110,
+        75, 125,
+        55, 20,
+        10, 0
     ]);
 
     stage.addChild(box);
