dcecb28c24291bc3f9364d9dc01504b75d72e1de
Eric Lifka <eric.lifka@inin.com>
2017-04-06 23:11:59 -0400

    drop in the gl demo from pxlr as a starting point

--- src/main.js
+++ src/main.js
@@ -1,1 +1,38 @@
-console.log('hi');
+SM.DefineModule('main', function (require) {
+  let RunLoop = require('pxlr/core/run-loop');
+  let Renderer = require('pxlr/gl/webgl');
+  let Sprite = require('pxlr/core/sprite');
+
+  let width = 250;
+  let height = 150;
+  let colorWidthStep = 256 / width;
+  let colorHeightStep = 256 / height;
+  let offset = 0;
+  let sprite = Sprite.newEmptySprite({width, height});
+
+  let renderer = new Renderer({width, height});
+  let runLoop = new RunLoop(function () {
+    let frame = renderer.newRenderFrame();
+
+    updateGradient();
+    sprite.renderToFrame(frame);
+
+    renderer.renderFrame(frame);
+  });
+
+  function updateGradient() {
+    offset = (offset + 1) % 256;
+
+    sprite.iterateCells((color, x, y) => {
+      if (x % 2 === 0 && y % 2 === 0) {
+        color.setR(0.0);
+        color.setG(0.0);
+      } else {
+        color.setR(x * colorWidthStep + offset);
+        color.setG(y * colorHeightStep + offset);
+      }
+    });
+  }
+
+  runLoop.start();
+});
