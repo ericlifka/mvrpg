0c18affa1d3a32622160f62f68c29241bdc95f91
Eric Lifka <eric.lifka@inin.com>
2017-04-10 23:55:27 -0400

    turn tile grid into the level manager class and move cursor under its ownership

--- src/game.js
+++ src/game.js
@@ -1,40 +1,16 @@
 SM.DefineModule('mvrpg/game', function (require) {
   const GameEntity = require('pxlr/core/game-entity');
 
-  const Cursor = require('mvrpg/components/cursor');
-  const TileGrid = require('mvrpg/screens/tile-grid');
+  const LevelManager = require('mvrpg/screens/level-manager');
 
   return SM.DefineClass([GameEntity, {
     constructor(dimensions) {
       this.dimensions = dimensions;
-      this.tileGrid = new TileGrid(this, dimensions);
-      this.cursor = new Cursor(this);
+      this.levelManager = new LevelManager(this, this.dimensions);
     },
 
     init: SM.event(function () {
-      this.children.push(this.tileGrid);
-      this.children.push(this.cursor);
-    }),
-
-    update: SM.event(function (dtime, inputSources) {
-      inputSources.forEach(input => {
-        if (input.INPUT_TYPE === "keyboard") {
-
-          if (input.W){
-            this.cursor.position.y += 32;
-          }
-          if (input.A) {
-            this.cursor.position.x -= 32;
-          }
-          if (input.S) {
-            this.cursor.position.y -= 32;
-          }
-          if (input.D) {
-            this.cursor.position.x += 32;
-          }
-
-        }
-      });
-    }),
+      this.children.push(this.levelManager);
+    })
   }]);
 });
--- src/screens/level-manager.js
+++ src/screens/level-manager.js
@@ -1,12 +1,13 @@
-SM.DefineModule('mvrpg/screens/tile-grid', function (require) {
+SM.DefineModule('mvrpg/screens/level-manager', function (require) {
   const GameEntity = require('pxlr/core/game-entity');
+  const Cursor = require('mvrpg/components/cursor');
   const grassSprite = require('mvrpg/tiles/basic-grass');
-  const Color = require('pxlr/core/RGB-color');
 
   return SM.DefineClass([GameEntity, {
 
     constructor(parent, dimensions) {
       this.dimensions = dimensions;
+      this.cursor = new Cursor(this);
 
       this.grid = [
         [ grassSprite(), grassSprite(), grassSprite(), grassSprite(), grassSprite(), grassSprite(), grassSprite(), grassSprite() ],
@@ -17,18 +18,32 @@ SM.DefineModule('mvrpg/screens/tile-grid', function (require) {
       ];
     },
 
-    renderToFrame: SM.event(function (frame) {
-      // let red = new Color(256, 0, 0);
-      //
-      // for (let w = 0; w <= this.dimensions.width; w++) {
-      //   for (let h = 0; h <= this.dimensions.height; h++) {
-      //     if (w % 32 === 0 || h % 32 === 0) {
-      //       let frameColor = frame.cellAt(w, h);
-      //       frameColor.copyFromColor(red);
-      //     }
-      //   }
-      // }
+    init: SM.event(function () {
+      this.children.push(this.cursor);
+    }),
 
+    update: SM.event(function (dtime, inputSources) {
+      inputSources.forEach(input => {
+        if (input.INPUT_TYPE === "keyboard") {
+
+          if (input.W){
+            this.cursor.position.y += 32;
+          }
+          if (input.A) {
+            this.cursor.position.x -= 32;
+          }
+          if (input.S) {
+            this.cursor.position.y -= 32;
+          }
+          if (input.D) {
+            this.cursor.position.x += 32;
+          }
+
+        }
+      });
+    }),
+
+    renderToFrame: SM.event(function (frame) {
       this.grid.forEach((row, y) => {
         row.forEach((sprite, x) => {
           sprite.renderToFrame(frame, x * 32, y * 32);
